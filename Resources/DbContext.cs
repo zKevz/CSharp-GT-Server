063650a38ebc012a96ec6a95f0e4ed2082b8e42a
zKevz <65283050+zKevz@users.noreply.github.com>
2020-07-25 19:05:46 +0700

    Update DbContext.cs

--- Resources/DbContext.cs
+++ Resources/DbContext.cs
@@ -5,7 +5,7 @@ namespace GTServer.Resources
 {
     public static class DbContext
     {
-        public static string ConnectionString { get; set; } = "server=localhost;uid=root;password=kevin1206;database=growtopiadb";
+        public static string ConnectionString { get; set; } = "server=localhost;uid=your-uid;password=your-password;database=your-db";
         public static MySqlConnection Connection { get; set; } = new MySqlConnection(ConnectionString);
         public static async Task OpenConnection() => await Connection.OpenAsync();
     }
